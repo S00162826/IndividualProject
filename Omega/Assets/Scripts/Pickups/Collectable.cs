9f2251fbe4eb4fb2bc92dfa9c04427e985c1baf1
S00162826 <S00162826@mail.itsligo.ie>
2018-07-23 16:07:56 +0100

    Collectables
    Wrote script to handle collectables, game seemed a bit empty so added collectables

---
+++ Omega/Assets/Scripts/Pickups/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectable : MonoBehaviour
+{
+    public float totalCollectables;
+    public float collectablesFound;
+    public Text collectables;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Collectable")
+            collectablesFound += 1;
+            Destroy(other.gameObject);
+    }
+        void Update()
+    {
+        collectables.text = "Collectables Found : " + collectablesFound.ToString()
+                            + "/" + totalCollectables.ToString();
+    }
+}
